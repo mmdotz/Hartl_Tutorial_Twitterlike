8d07b0748064f9319ed9fdcef367ba9d317b27ba
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-01-10 13:24:35 -0600

    updating static pages with helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | HartlTwitterlike"
+    assert_select "title", "HartlTwitterlike"
   end
 
   test "should get help" do
