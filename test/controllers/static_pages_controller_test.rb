683a032cad1f3f59a6d5eaa8875327d31f5fc8ee
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-01-10 12:56:27 -0600

    testing html tags are selected

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial"
+    assert_select "title", "Home | HartlTwitterlike"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial"
+    assert_select "title", "Help | HartlTwitterlike"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial"
+    assert_select "title", "About | HartlTwitterlike"
   end
 
 end
