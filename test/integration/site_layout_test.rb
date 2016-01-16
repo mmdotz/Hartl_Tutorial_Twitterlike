419e5c775ef71e0692f4520bb599e0985639f763
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-01-16 10:26:42 -0600

    test layout, gen user

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,12 +5,14 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
+    puts response.body
+
     assert_template 'static_pages/home'
     #rails inserts value of _path in place of ?
     #specifically looking for <a href="/about">...</a>
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", about_path
   end
 end
